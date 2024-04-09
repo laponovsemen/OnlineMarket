e93d6d0049283405e018ba5df9a47698baba638e
Laponov Semen <johndoe@example.com>
2024-04-09 18:51:25 +0400

    creating ThemeProvider.tsx and ThemeContext.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom'
-import "./../index.scss"
+import "./styles/index.scss"
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+
 
 export const App = () => {
+	const {theme, toggleTheme} = useTheme()
 	return (
-		<div className="app">
-		<Link to={"/about"}>Главная страница</Link>
-		<Link to={'/'}>О сайте</Link>
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>TOGGLE</button>
+			<Link to={"/about"}>Главная страница</Link>
+			<Link to={'/'}>О сайте</Link>
 			<Suspense fallback={"Loading..."}>
 				<Routes>
 					<Route path={'/about'} element={<AboutPageAsync/>}/>
 					<Route path={'/'} element={<MainPageAsync/>}/>
 				</Routes>
 			</Suspense>
-
 
 
 		</div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import {render} from "react-dom";
 import {Counter} from "./components/Counter";
 import {App} from "./App";
 import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 // import React from "react"; в 17 версии реакта по дефолту импорт реакта не нужен решение в тс конфиге
 // <BrowserRouter/> // для навигации по странице в спа
 render(
 	<BrowserRouter>
-		<App/>
+		<ThemeProvider>
+			<App/>
+		</ThemeProvider>
+
 	</BrowserRouter>
 	,
 	document.getElementById('root')
