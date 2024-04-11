2891d10e61abe06fa4207cf96d847b4098082796
Laponov Semen <johndoe@example.com>
2024-04-11 13:21:24 +0400

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     //  декларации в тсконфиг в поле "includes"
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true
     },
     "extends": [
         "eslint:recommended",
---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,38 @@
+import {classNames} from "./classNames";
+
+describe("classNames", () => {
+    test("with only first param", () => {
+        expect(true).toBe(true);
+        expect(classNames("someClass")).toBe("someClass");
+    });
+
+    test("with additional classes", () => {
+        const expected = "someClass class1 class2";
+        expect(classNames("someClass", {}, ["class1", "class2"]))
+            .toBe(expected);
+    });
+    test("with additional classes and mods", () => {
+        const expected = "someClass class1 class2 hovered scrollable";
+        expect(classNames(
+            "someClass",
+            {hovered: true, scrollable: true},
+            ["class1", "class2"]))
+            .toBe(expected);
+    });
+    test("with additional classes and mods but one of mods is false", () => {
+        const expected = "someClass class1 class2 hovered";
+        expect(classNames(
+            "someClass",
+            {hovered: true, scrollable: false},
+            ["class1", "class2"]))
+            .toBe(expected);
+    });
+    test("with additional classes and mods but one of mods is undefined", () => {
+        const expected = "someClass class1 class2 hovered";
+        expect(classNames(
+            "someClass",
+            {hovered: true, scrollable: undefined},
+            ["class1", "class2"]))
+            .toBe(expected);
+    });
+});
